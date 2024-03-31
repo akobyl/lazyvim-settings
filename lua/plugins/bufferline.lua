142593e00986dd1e1137cfeb39250a2f330f75ff
Andy Kobyljanec <kobyl125@gmail.com>
2024-03-31 06:50:28 -0400

    Initial commit from default lazyvim settings

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,7 @@
+return {
+  "akinsho/bufferline.nvim",
+  keys = {
+    { "<leader>gh", "<cmd>BufferLinePick<CR>", desc = "Pick buffer" },
+    { "<leader>gq", "<cmd>BufferLinePickClose<CR>", desc = "Pick buffer to close" },
+  },
+}
---
+++ lua/plugins/themes.lua
@@ -0,0 +1,6 @@
+return {
+  { "sainnhe/everforest" },
+  { "sainnhe/gruvbox-material" },
+  { "kepano/flexoki" },
+  { "olimorris/onedarkpro.nvim" },
+}
